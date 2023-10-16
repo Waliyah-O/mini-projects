903ee1a4341ccf1fcab297feaf18cca6d7309022
Waliyah <129737697+Waliyah-O@users.noreply.github.com>
2023-10-16 13:03:32 +0100

    Add files via upload

--- content place holder/script.js
+++ content place holder/script.js
@@ -11,24 +11,24 @@ const animated_bg_text = document.querySelectorAll(".animated-bg-text");
 setTimeout(getData, 3000);
 
 async function getData() {
-//   const request1 = fetch("https://randomuser.me/api").then((res) => res.json());
-//   const request2 = fetch("https://jsonplaceholder.typicode.com/posts/2").then(
-//     (res) => res.json()
-//   );
+  //   const request1 = fetch("https://randomuser.me/api").then((res) => res.json());
+  //   const request2 = fetch("https://jsonplaceholder.typicode.com/posts/2").then(
+  //     (res) => res.json()
+  //   );
 
-//   Promise.all([request1, request2])
-//     .then(([data1, data2]) => {
-//       console.log(data2.title);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
+  //   Promise.all([request1, request2])
+  //     .then(([data1, data2]) => {
+  //       console.log(data2.title);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
 
   header.innerHTML = `<img src="https://source.unsplash.com/featured/300x201" alt="unsplash"/>`;
 
   const adviceRes = await fetch("https://api.adviceslip.com/advice");
   const adviceJson = await adviceRes.json();
-  //   console.log(adviceJson);
+  // console.log(adviceJson);
   excerpt.innerHTML = `${adviceJson.slip.advice}`;
 
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/2");
@@ -38,7 +38,7 @@ async function getData() {
 
   const res = await fetch("https://randomuser.me/api");
   const { results } = await res.json();
-  //   console.log(results);
+  console.log(results);
   results.forEach((user) => {
     profileImg.innerHTML = `<img src="${user.picture.large}" src="${user.name.first}"/>`;
     name.innerHTML = `${user.name.first} ${user.name.last}`;
